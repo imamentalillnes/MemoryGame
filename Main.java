12be735dcb0d58c78c872ae8385715308e117533
ima <mabruns0923@gmail.com>
2025-03-11 13:51:16 -0400

    I got buttons to print to a file so the scanner can later read it to check inputed numbers against the list of numbers...kill me

--- Main.java
+++ Main.java
@@ -1,6 +1,42 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Window game = new Window("Game");
+
+        //Creates file to read the buttons pressed from
+        try{
+            File numIn = new File("NumbersIn.txt");
+            Scanner scnr = new Scanner(numIn);
+
+            //Window method call
+            Window game = new Window("Game", numIn);
+
+            //Variables
+            ArrayList<Integer> buttonCheck = game.getButtons();
+            
+            
+            //game loop
+            while(game.getPlaying()){
+                int num = scnr.nextInt();
+                if(num == buttonCheck.get(0)){
+                    System.out.println("You got it right!");
+                }
+                else{
+                    System.out.println("wrong!");
+                    game.setLife();
+                }
+            }
+
+            scnr.close();
+            //writer.close();
+        }
+        catch(IOException ioe){
+            System.out.println("file failed to be made terminating process");
+            System.out.println(ioe);
+            return;
+        }
     }
 }
--- Window.java
+++ Window.java
@@ -1,35 +1,126 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
+import java.io.*;
+import java.util.*;
 
 public class Window extends JFrame{
 
     //Base Variables needed through the entire program
-    int frameSize = 4;
-    JPanel basePanel = new JPanel(new BorderLayout());
-    JPanel gameField = new JPanel(new GridLayout(frameSize, frameSize));
+    private int frameSize = 4;
+    private int life = 3;
+    private int level = 1;
+    private boolean playing = true;
+    private JPanel basePanel = new JPanel(new BorderLayout());
+    private JPanel gameField = new JPanel(new GridLayout(frameSize, frameSize));
+    private Random rng = new Random();
+    
     
 
-    public Window(String Name){
+    public Window(String Name, File writer) throws IOException{
         //Calls the super constructor
         super(Name);
 
         //sets up the frame an desired 
         this.setSize(500, 500);
+        this.setLocation(500, 250);
         this.gameField.setPreferredSize(new Dimension(500, 500));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(basePanel);
         basePanel.add(gameField, BorderLayout.CENTER);
         this.setVisible(true);
 
-        //Makes all the buttons for the current stage
+        //calls the makeButton method
+        makeButtons(writer);
+    }
+
+    //Getters and Setters
+    public Integer getFrameSize() {
+        return frameSize;
+    }
+
+    public void setFrameSize(int num){
+        if(frameSize < num){
+            frameSize = num;
+        }
+    }
+
+    public boolean getPlaying() {
+        if(life <= 0){
+            playing = false;
+        }
+        return playing;
+    }
+
+    public void setLife(){
+        life -= 1;
+    }
+
+
+    //Makes all the buttons for the current stage
+    private void makeButtons(File writer){
+
+        //creates the buttons and adds them to the plain
         for(int i = 0; i < (frameSize * frameSize); ++i){
-            Button press = new Button("" + i);
+            int buttonID = i;
+            Button press = new Button();
             press.addActionListener(e -> {
-                System.out.println("you pressed button " + press.getLabel());
+                try{
+                    FileWriter reWriter = new FileWriter(writer, true);
+                    reWriter.write(buttonID + " ");
+                    System.out.println("Button " + buttonID + " was here");
+                    reWriter.close();
+                }
+                catch(IOException ioe){
+                    System.out.println(ioe);
+                    System.out.println("Could not write file");
+                }
+                
             });
             press.setVisible(true);
             gameField.add(press);
         }
+
+    }
+
+
+    public ArrayList<Integer> getButtons(){
+
+        //makes arraylist for the buttons that need memorized
+        ArrayList<Integer> buttons = new ArrayList<Integer>();
+
+        //adds in all the buttons that are checked for
+        while(buttons.size() < level){
+
+            //variables for check and making ran numbers
+            boolean used = false;
+            int num = rng.nextInt((frameSize*frameSize));
+
+            //checks if there is anything in the arraylist already
+            if (buttons.size() == 0){
+                buttons.add(num);
+            }
+
+            //if so it then checks if the same number is being added again
+            else {
+                for( int a: buttons){
+                    if(a == num){
+                        used = true;
+                    }
+                }
+                if (!used){
+                    buttons.add(num);
+                }
+            }
+        }
+
+
+        //DELETE THIS 
+        for(int a : buttons){
+            System.out.println(a);
+        }
+        System.out.println("nums in the list");
+
+        //returns the list
+        return buttons;
     }
 }
