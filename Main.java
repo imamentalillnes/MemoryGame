3cbec7b7a4043331f2877bd5ae826b9cd73a7a33
ima <mabruns0923@gmail.com>
2025-03-12 00:04:04 -0400

    IT'S WORKING!!!!!! need to add a easy restart button and add more comments this is confusing ;-;

--- Main.java
+++ Main.java
@@ -1,40 +1,16 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+public class Main{
 
-public class Main {
+    public static void main(String[] args) throws InterruptedException {
 
-    public static void main(String[] args) {
+        //Window method call
+        Window game = new Window("Game");
 
-        //Creates file to read the buttons pressed from
-        try{
-            File numIn = new File("NumbersIn.txt");
-            Scanner scnr = new Scanner(numIn);
-
-            //Window method call
-            Window game = new Window("Game", numIn);
-
-            //Variables
-            ArrayList<Integer> buttonCheck = game.getButtons();
-            
-            
-            //game loop
-            while(game.getPlaying()){
-                while (scnr.hasNextInt()){
-                    if(scnr.nextInt() == buttonCheck.get(0)){
-                        System.out.println("You got it right!");
-                    }
-                    else{
-                        System.out.println("wrong!");
-                        game.setLife();
-                    }
-                }
+        while(game.getPlaying()){
+            Thread.sleep(500);
+            if(game.getPressed() == game.getNumList()){
+                game.winLevel();
             }
         }
-        catch(IOException ioe){
-            System.out.println("file failed to be made terminating process");
-            System.out.println(ioe);
-            return;
-        }
     }
+
 }
--- Window.java
+++ Window.java
@@ -1,9 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.*;
 import java.util.*;
 
 public class Window extends JFrame{
+
+    /*
+    //method to make the numslist
+    private ArrayList<Integer> makeNumsList() throws InterruptedException {
+        ArrayList<Integer> numsList = getButtons();
+        return numsList;
+    }    
+        */
 
     //Base Variables needed through the entire program
     private int frameSize = 4;
@@ -12,11 +19,13 @@ public class Window extends JFrame{
     private boolean playing = true;
     private JPanel basePanel = new JPanel(new BorderLayout());
     private JPanel gameField = new JPanel(new GridLayout(frameSize, frameSize));
+    private JLabel lives = new JLabel("Lives: " + life);
     private Random rng = new Random();
-    
-    
+    private int pressed = 0;
+    private ArrayList<JButton> gameButtons = new ArrayList<>();
+    private ArrayList<Integer> numsList;
 
-    public Window(String Name, File writer) throws IOException{
+    public Window(String Name){
         //Calls the super constructor
         super(Name);
 
@@ -27,10 +36,19 @@ public class Window extends JFrame{
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(basePanel);
         basePanel.add(gameField, BorderLayout.CENTER);
+        basePanel.add(lives, BorderLayout.NORTH);
         this.setVisible(true);
+        this.setAlwaysOnTop(true);
 
         //calls the makeButton method
-        makeButtons(writer);
+        try{
+            makeButtons();
+        }
+        catch(InterruptedException IE){
+            System.out.println(IE);
+            System.out.println("Line 44");
+        }
+        
     }
 
     //Getters and Setters
@@ -52,40 +70,68 @@ public class Window extends JFrame{
     }
 
     public void setLife(){
-        life -= 1;
+        if(life > 0){
+            life -= 1;
+        }
+    }
+
+    public int getPressed(){
+        return pressed;
+    }
+
+    public int getNumList() {
+        return numsList.size();
     }
 
 
     //Makes all the buttons for the current stage
-    private void makeButtons(File writer) throws IOException{
-        FileWriter clear = new FileWriter(writer);
-        clear.write("");
+    private void makeButtons() throws InterruptedException{
+
+        numsList = getButtons();
 
         //creates the buttons and adds them to the plain
         for(int i = 0; i < (frameSize * frameSize); ++i){
             int buttonID = i;
-            Button press = new Button();
+            JButton press = new JButton();
+            press.setBackground(Color.white);
+            gameButtons.add(press);
+
+            //Action Listener Creation
             press.addActionListener(e -> {
-                try{
-                    FileWriter reWriter = new FileWriter(writer, true);
-                    reWriter.write(buttonID + " ");
-                    System.out.println("Button " + buttonID + " was here");
-                    reWriter.close();
+
+                if(life <= 0){
+                    lives.setText("Lives: Dead");
+                    
+                    for(JButton a : gameButtons){
+                        a.setBackground(Color.gray);
+                        a.setEnabled(false);
+                    }
                 }
-                catch(IOException ioe){
-                    System.out.println(ioe);
-                    System.out.println("Could not write file");
+                else{
+                    if(buttonID == numsList.get(pressed)){
+                        press.setBackground(Color.GREEN);
+                        curClean();
+                        pressed++;
+                    }
+                    else{
+                        press.setBackground(Color.RED);
+                        this.setLife();
+                        lives.setText("Lives: " + life);
+                    }
                 }
-                
             });
+
             press.setVisible(true);
             gameField.add(press);
         }
 
+        //shows the required button to remember
+        showRemember();
+
     }
 
 
-    public ArrayList<Integer> getButtons(){
+    private ArrayList<Integer> getButtons(){
 
         //makes arraylist for the buttons that need memorized
         ArrayList<Integer> buttons = new ArrayList<Integer>();
@@ -95,15 +141,12 @@ public class Window extends JFrame{
 
             //variables for check and making ran numbers
             boolean used = false;
-            int num = rng.nextInt((frameSize*frameSize));
 
-            //checks if there is anything in the arraylist already
-            if (buttons.size() == 0){
-                buttons.add(num);
-            }
+            do{
+                used = false;
+                int num = rng.nextInt((frameSize*frameSize));
 
-            //if so it then checks if the same number is being added again
-            else {
+                //checks if the same number is being added again
                 for( int a: buttons){
                     if(a == num){
                         used = true;
@@ -111,18 +154,65 @@ public class Window extends JFrame{
                 }
                 if (!used){
                     buttons.add(num);
+                    
                 }
-            }
+            }while(used);
         }
-
-
-        //DELETE THIS 
-        for(int a : buttons){
-            System.out.println(a);
-        }
-        System.out.println("nums in the list");
 
         //returns the list
         return buttons;
     }
+
+    private void showRemember(){
+
+        try{
+        //shows which buttons were chosen
+        for(int a : numsList){
+            gameButtons.get(a).setBackground(Color.YELLOW);
+            System.out.println(a);
+            Thread.sleep(1000);
+        }
+        System.out.println("nums in the list");
+        }
+        catch(InterruptedException IE){
+            System.out.println("showRemeber Error");
+        }
+
+        clean();
+    }
+
+    private void clean() {
+        //resets the button colors
+        for(JButton a: gameButtons){
+            a.setBackground(Color.white);
+        }
+    }
+
+    private void curClean() {
+        for(JButton a : gameButtons){
+            if(a.getBackground() == Color.RED){
+                a.setBackground(Color.white);
+            }
+        }
+    }
+
+    public void winLevel() {
+
+        //once the round is Complete it resets everything and moves up a level
+            level++;
+            numsList = getButtons();
+            clean();
+            pressed = 0;
+
+            try{
+                Thread.sleep(1000);
+                showRemember();
+            }
+            catch(InterruptedException ie){
+                System.out.println(ie);
+            }
+
+    }
+
+    
 }
